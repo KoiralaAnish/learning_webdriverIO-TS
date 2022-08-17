707ceca2c78f44ac7fe6676c7ea63773aa236ec9
koianish <koiralaanish47@gmail.com>
2022-08-17 13:05:12 +0545

    updated sauce page

--- test/pageobjects/sauceDemo/sauce.ts
+++ test/pageobjects/sauceDemo/sauce.ts
@@ -1,5 +1,5 @@
 export default class {
   public open(path: string) {
-    return browser.url(`https://www.saucedemo.com//${path}`);
+    return browser.url(`https://www.saucedemo.com${path}`);
   }
 }
